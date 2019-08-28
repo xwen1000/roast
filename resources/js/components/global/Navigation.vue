e3718e4572504a10bdd936c72fc897efdfeb9d25
xingwen <xwen100@qq.com>
2019-08-28 08:36:29 +0800

    Home

---
+++ resources/js/components/global/Navigation.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <nav class="top-navigation">
+            <router-link :to="{ name: 'home'}">
+                <span class="logo">Roast</span>
+            </router-link>
+            <ul class="links">
+            <li>
+                <router-link :to="{ name: 'cafes' }">
+                    Cafes
+                </router-link>
+            </li>
+        </ul>
+        <div class="right">
+            <img class="avatar" :src="user.avatar" v-show="userLoadStatus == 2"/>
+        </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+        computed: {
+            userLoadStatus(){
+                return this.$store.getters.getUserLoadStatus;
+            },
+            user(){
+                return this.$store.getters.getUser;
+            }
+        }
+}
+</script>
+
+<style scoped>
+
+</style>
